67296af85c63c11624a3a58c9e855a62349f128c
anshul akotiya <anshul90akotiya@gmail.com>
2022-12-30 13:32:37 +0530

    anshul

--- help_app/static/js/main.js
+++ help_app/static/js/main.js
@@ -25,6 +25,33 @@ function validateEmail(){
                         document.getElementById('otpRow').hidden = false;
                         document.getElementById('otpButtonRow').hidden = false;
                         document.getElementById('validate-button').hidden = true;
+                        document.getElementById("resend-button").disabled=true;
+                        var sec         = 120,
+                            countDiv    = document.getElementById("resend-button"),
+                            countDown   = setInterval(function () {
+                                'use strict';
+                                secpass();
+                            }, 1000);
+                    function secpass() {
+                        'use strict';
+                            var min     = Math.floor(sec / 60),
+                                remSec  = sec % 60;
+
+                            if (remSec < 10) {
+                                remSec = '0' + remSec;
+                            }
+                            if (min < 10) {
+                                min = '0' + min;
+                            }
+                            countDiv.innerHTML = min + ":" + remSec;
+                            if (sec > 0) {
+                                sec = sec - 1;
+                            } else {
+                                clearInterval(countDown);
+                                countDiv.disabled = false;
+                                countDiv.innerHTML = 'Resend OTP';
+                            }
+                        }
                     }
                 }
             });
