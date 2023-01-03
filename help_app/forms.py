be7cccb5c43b3fb6a4c7ed9e708ab3162549d112
akotiyaanshul <akotiyaanshul@gmail.com>
2023-01-03 17:18:49 +0530

    anshul

--- help_app/forms.py
+++ help_app/forms.py
@@ -7,6 +7,10 @@ class BranchForm(forms.ModelForm):
     class Meta:
         model = Branch
         fields = "__all__"
+        widgets = {
+            'branch_name': forms.DateInput(attrs={'placeholder': 'Branch Name'}),
+            'branch_code': forms.DateInput(attrs={'placeholder': 'Branch Code (3 Letters Only)'}),
+        }
 
     def clean_branch_code(self):
         return self.cleaned_data["branch_code"].upper()
